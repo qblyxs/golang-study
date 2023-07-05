16cf7ab37315e8651a038cdcd533224b040dc07e
qblyxs <qblyxs@qq.com>
2023-07-05 15:53:23 +0800

    23-07-05-(2)

--- test/interface.go
+++ test/interface.go
@@ -15,6 +15,9 @@ type Chinese struct {
 func (c Chinese) SayHi() {
 	fmt.Println("你好,我是中国人")
 }
+func (c Chinese) niuYang() {
+	fmt.Println("我会中国文化")
+}
 
 
 // 定义American结构体,实现SayHi()方法
@@ -23,17 +26,33 @@ type American struct {
 func (a American) SayHi() {
 	fmt.Println("Hello,I'm American")
 }
+func (a American) playBasketball() {
+	fmt.Println("我会打篮球")
+}
 
 
 // 定义一个函数,参数为接口类型
 func SayHiFunc(s SayHi) {
 	s.SayHi()
-	fmt.Println("SayHiFunc函数执行完毕")
-	ch,ok := s.(Chinese) // 类型断言
-	if ok {
-		fmt.Println("是中国人")
-		fmt.Println(ch)
+	fmt.Println("---------断言---------")
+	// ch,ok := s.(Chinese) // 类型断言
+	// if ok {
+	if ch,ok := s.(Chinese); ok {
+		ch.niuYang()
+		// fmt.Println(ch)
 	} else {
-		fmt.Println("不是中国人")
+		am := s.(American)
+		am.playBasketball()
+	}
+	fmt.Println("------------------")
+	switch s.(type) {
+	case Chinese:
+		ch := s.(Chinese)
+		ch.niuYang()
+	case American:
+		am := s.(American)
+		am.playBasketball()
+	default:
+		fmt.Println("我是外星人")
 	}
 }
